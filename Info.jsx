440e126dfd3fb29e2f3539063c752d6bf37b5fc3
indhumathi1511 <86593139+indhumathi1511@users.noreply.github.com>
2021-06-28 13:11:35 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and Frontend with ReactJS </h1>
+      <p>A basic Web dev ReactJS Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
